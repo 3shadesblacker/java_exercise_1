90b8b3176b78aaea49efe3b103b8162668149495
Mouhamad Al Moustapha MBAYE <mouhamad-al-moustapha.mbaye@epita.fr>
2021-05-17 18:40:12 +0200

    Part4

--- src/Launcher.java
+++ src/Launcher.java
@@ -17,8 +17,33 @@ public class Launcher
         }*/
         while (!s.equals("quit"))
         {
-            System.out.println("Unknown command. Try again: ");
-            s = sc.nextLine();
+            if (s.equals("fibo"))
+            {
+                int n;
+                System.out.println("Type the index value: ");
+                n = sc.nextInt();
+                if (n == 0 || n == 1)
+                {
+                    System.out.println(n);
+                }
+                else
+                {
+                    int a = 0, b = 1, c = a + b;
+                    for (int i = 2; i < n; i++)
+                    {
+                        a = b;
+                        b = c;
+                        c = a + b;
+                    }
+                    System.out.println(c);
+                }
+                s = sc.nextLine();
+            }
+            else
+            {
+                System.out.println("Type in your command: ");
+                s = sc.nextLine();
+            }
         }
         System.exit(0);
     }

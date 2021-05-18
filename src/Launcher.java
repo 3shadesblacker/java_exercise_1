5f8af5cf74012fce9e9c9b983cad18338971be70
Mouhamad Al Moustapha MBAYE <mouhamad-al-moustapha.mbaye@epita.fr>
2021-05-18 23:02:49 +0200

    freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
+import java.nio.file.*;
+import java.io.IOException;
 
 public class Launcher
 {
@@ -6,15 +8,7 @@ public class Launcher
     {
         System.out.println("Hello World ! Type in your command: ");
         Scanner sc = new Scanner( System.in );
-        String s = sc.nextLine();/*
-        if (s.equals("quit"))
-        {
-            System.exit(0);
-        }
-        else
-        {
-            System.out.println("Unknown command");
-        }*/
+        String s = sc.nextLine();
         while (!s.equals("quit"))
         {
             if (s.equals("fibo"))
@@ -37,13 +31,59 @@ public class Launcher
                     }
                     System.out.println(c);
                 }
-                s = sc.nextLine();
+            }
+            else if (s.equals("freq"))
+            {
+                System.out.println("Type in the path to your file: ");
+                Path p = Path.of(sc.nextLine());
+                try
+                {
+                    String P = Files.readString(p);
+                    String[] S = P.split(" ");
+                    HashMap<String, Integer> h = new HashMap<>();
+                    for (int i = 0; i < S.length; i++)
+                    {
+                        if (h.containsKey(S[i]))
+                        { h.put(S[i], h.get(S[i]) + 1); }
+                        else
+                        { h.put(S[i], 1); }
+                    }
+                    Set<Map.Entry<String, Integer> > set = h.entrySet();
+                    String A = "", B = "", C = "";
+                    int a = 0, b = 0, c = 0;
+                    for (Map.Entry<String, Integer> me : set)
+                    {
+                        if (me.getValue() > a)
+                        {
+                            a = me.getValue();
+                            c = b; C = B;
+                            b = a; B = A;
+                            A = me.getKey();
+                        }
+                        else if (me.getValue() > b)
+                        {
+                            b = me.getValue();
+                            c = b; C = B;
+                            B = me.getKey();
+                        }
+                        else
+                        {
+                            c = me.getValue();
+                            C = me.getKey();
+                        }
+                    }
+                    System.out.println("1. " + A + " 2. " + B + " 3. " + C);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
             }
             else
             {
                 System.out.println("Type in your command: ");
-                s = sc.nextLine();
             }
+            s = sc.nextLine();
         }
         System.exit(0);
     }
